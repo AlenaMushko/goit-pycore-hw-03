c880990e0d6a139b94f990c000a8c13ede64b636
alena <108529764+AlenaMushko@users.noreply.github.com>
2025-03-09 12:08:48 +0200

    add logic to the get_upcoming_birthdays

--- phone_number.py
+++ phone_number.py
@@ -1,10 +1,14 @@
 import re
 
 def normalize_phone(phone_number: str) -> str:
-    pass
+    pattern = re.compile(r'\D')
+    clean_phone_number= re.sub(pattern, '',  phone_number)
+
+    correct_phone_number =  re.sub(r'^(?:380|80|0)?', '+380', clean_phone_number)
+    return correct_phone_number
 
 raw_numbers = [
-    "067\\t123 4567",
+    "067\\t123 45+67",
     "(095) 234-5678\\n",
     "+380 44 123 4567",
     "380501234567",
@@ -13,6 +17,8 @@ raw_numbers = [
     "(050)8889900",
     "38050-111-22-22",
     "38050 111 22 11   ",
+    "80-    12345.  67",
+    "880-----1234567"
 ]
 
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
--- upcoming_birthdays.py
+++ upcoming_birthdays.py
@@ -1,25 +1,47 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 from typing import List, Dict
 
 def get_upcoming_birthdays(users: List[Dict[str, str]]) -> List[Dict[str, str]]:
-    pass
+    current_date = datetime.today().date()
+    date_after_7_days = current_date + timedelta(days=7)
+    upcoming_birthdays = []
+
+    for user in users:
+        user_birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        user_birthday_this_year = date(current_date.year, user_birthday.month, user_birthday.day)
+
+        if(current_date <= user_birthday_this_year <= date_after_7_days):
+            user_birthday_day_week = user_birthday_this_year.weekday()
+            if (user_birthday_day_week == 5):
+                upcoming_birthdays.append({
+                    "name": user["name"],
+                    "congratulation_date": (user_birthday_this_year + timedelta(days=2)).strftime("%Y.%m.%d")
+                })
+            elif (user_birthday_day_week == 6):
+                upcoming_birthdays.append({
+                    "name": user["name"],
+                    "congratulation_date": (user_birthday_this_year + timedelta(days=1)).strftime("%Y.%m.%d"),
+                })
+            else:
+                upcoming_birthdays.append({
+                    "name": user["name"],
+                    "congratulation_date": user_birthday_this_year.strftime("%Y.%m.%d"),
+                })
+
+    return upcoming_birthdays
 
 users = [
-    {"name": "John Doe", "birthday": "1985.03.09"},
-    {"name": "Jane Smith", "birthday": "1990.03.10"},
+    {"name": "John Doe", "birthday": "1985.03.06"},
+    {"name": "Jane Smith", "birthday": "1990.03.09"},
     {"name": "Michael Brown", "birthday": "1987.03.11"},
-    {"name": "Emily White", "birthday": "1995.03.12"},
-    {"name": "David Wilson", "birthday": "1992.03.13"},
-    {"name": "Sophia Miller", "birthday": "1988.03.14"},
-    {"name": "Daniel Taylor", "birthday": "1991.03.15"},
-    {"name": "Olivia Thomas", "birthday": "1993.03.16"},
-    {"name": "James Anderson", "birthday": "1986.03.17"},
-    {"name": "Charlotte Martinez", "birthday": "1994.03.18"},
+    {"name": "Emily White", "birthday": "1995.03.14"},
+    {"name": "David Wilson", "birthday": "1992.03.16"},
+    {"name": "Sophia Miller", "birthday": "1988.03.17"},
+    {"name": "Daniel Taylor", "birthday": "1991.03.23"},
+    {"name": "Olivia Thomas", "birthday": "1993.03.30"},
+    {"name": "James Anderson", "birthday": "1986.04.01"},
+    {"name": "Charlotte Martinez", "birthday": "1994.04.04"},
 ]
-
-upcoming_birthdays = get_upcoming_birthdays(users)
-print("Список привітань на цьому тижні:", upcoming_birthdays)
-
 
 upcoming_birthdays = get_upcoming_birthdays(users)
 print("Список привітань на цьому тижні:", upcoming_birthdays)
