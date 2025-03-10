38ca8be4836686c6912db44d083c075ccff9068f
alena <108529764+AlenaMushko@users.noreply.github.com>
2025-03-10 09:00:43 +0200

    update 2 & 4 foo

--- ticket_lottery.py
+++ ticket_lottery.py
@@ -1,15 +1,17 @@
 import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int)->list:
+    if quantity > (max - min + 1):    #  Питання до ментора - як краще повертати помилку чи порожній масив?
+        return []
     if not ( 1<= min < max <= 1000):
-        raise []
+        return []
     if not (min <= quantity <= max):
-        raise []
+        return []
 
     random_list = random.sample(range(min, max + 1), quantity)
     return sorted(random_list)
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(6, 9, 7)
 print(f'Ваші виграшні номери: {lottery_numbers}')
 
 
--- upcoming_birthdays.py
+++ upcoming_birthdays.py
@@ -2,7 +2,8 @@ from datetime import date, datetime, timedelta
 from typing import List, Dict
 
 def get_upcoming_birthdays(users: List[Dict[str, str]]) -> List[Dict[str, str]]:
-    current_date = datetime.today().date()
+    # current_date = datetime.today().date()
+    current_date = date(2024, 12, 29)
     date_after_7_days = current_date + timedelta(days=7)
     upcoming_birthdays = []
 
@@ -10,14 +11,17 @@ def get_upcoming_birthdays(users: List[Dict[str, str]]) -> List[Dict[str, str]]:
         user_birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
         user_birthday_this_year = date(current_date.year, user_birthday.month, user_birthday.day)
 
-        if(current_date <= user_birthday_this_year <= date_after_7_days):
+        if user_birthday_this_year < current_date and date_after_7_days.month == 1 and user_birthday.month == 1:
+            user_birthday_this_year = date(current_date.year + 1, user_birthday.month, user_birthday.day)
+
+        if current_date <= user_birthday_this_year <= date_after_7_days:
             user_birthday_day_week = user_birthday_this_year.weekday()
             if (user_birthday_day_week == 5):
                 upcoming_birthdays.append({
                     "name": user["name"],
                     "congratulation_date": (user_birthday_this_year + timedelta(days=2)).strftime("%Y.%m.%d")
                 })
-            elif (user_birthday_day_week == 6):
+            elif user_birthday_day_week == 6:
                 upcoming_birthdays.append({
                     "name": user["name"],
                     "congratulation_date": (user_birthday_this_year + timedelta(days=1)).strftime("%Y.%m.%d"),
@@ -31,9 +35,9 @@ def get_upcoming_birthdays(users: List[Dict[str, str]]) -> List[Dict[str, str]]:
     return upcoming_birthdays
 
 users = [
-    {"name": "John Doe", "birthday": "1985.03.06"},
-    {"name": "Jane Smith", "birthday": "1990.03.09"},
-    {"name": "Michael Brown", "birthday": "1987.03.11"},
+    {"name": "John Doe", "birthday": "1985.12.29"},
+    {"name": "Jane Smith", "birthday": "1990.01.01"},
+    {"name": "Michael Brown", "birthday": "1987.01.04"},
     {"name": "Emily White", "birthday": "1995.03.14"},
     {"name": "David Wilson", "birthday": "1992.03.16"},
     {"name": "Sophia Miller", "birthday": "1988.03.17"},
